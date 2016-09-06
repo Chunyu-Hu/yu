2b87b3f6c8bf2858701d4fcd68a029f84287b433
Chunyu Hu <chuhu@redhat.com>
2016-09-06 17:57:02 +0800

    kmods: Add workqueue operation

--- kmods/base_lib/base_lib.c
+++ kmods/base_lib/base_lib.c
@@ -5,41 +5,71 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/rcupdate.h>
+#include <linux/workqueue.h>
+#include <linux/kthread.h>
+#include <linux/device.h>
 
 static int dummy = 0;
 extern struct list_head tasks;
+static struct workqueue_struct *test_cleanup_wq;
+static int really_do = 0;
+
+static void deferred_probe_work_func(struct work_struct *work)
+{
+        struct device *dev;
+        pr_info("base_lib: in queued func: sizeof struct device: %ld",
+                sizeof(*dev));
+}
+
+static DECLARE_WORK(deferred_probe_work, deferred_probe_work_func);
 
 struct test_description {
-	int id;
-	int duration;
-	char res[8];
-	int date;
-	#ifdef __GENKSYMS__
-	int hiddend_data1;
-	int hiddend_data2;
-	int hiddend_data3;
-	#endif
+        int id;
+        int duration;
+        char res[8];
+        int date;
+        #ifdef __GENKSYMS__
+        int hiddend_data1;
+        int hiddend_data2;
+        int hiddend_data3;
+        #endif
 };
 
 static struct test_description base_lib_desc;
 
 EXPORT_SYMBOL(base_lib_desc);
 
+static void test_defer_it(struct work_struct* work)
+{
+	    if (!really_do)
+		        return;
+        /*
+         * Kick the re-probe thread.  It may already be scheduled, but it is
+         * safe to kick it again.
+         */
+	    queue_work(test_cleanup_wq, &deferred_probe_work);
+}
+
+int test_init_workqueue(void)
+{
+        test_cleanup_wq = create_singlethread_workqueue("test_cleanups");
+        if (WARN_ON(!test_cleanup_wq))
+		        return -ENOMEM;
+        return 0;
+}
+
 unsigned long long test_dump_tasklist(int number, pid_t pid)
 {
         struct task_struct *task;
-
         /* Protect the task struct during the iter */
 	    rcu_read_lock();
-
         for_each_process(task)
         {
-                printk("%s [%d], threads %d\n",task->comm , task->pid,
+                printk("%s [%d], threads %d\n",task->comm ,task->pid,
                         get_nr_threads(task));
                 if (!number--) 
                         break;
         }
-
 	    rcu_read_unlock();
 
         return number;
@@ -62,12 +92,13 @@ EXPORT_SYMBOL(test_dump_nodelist);
 
 int init_lib(void)
 {
-	return 0;
+        test_defer_it(&deferred_probe_work);
+	    return 0;
 }
 
 void exit_lib(void)
 {
-	return;
+	    return;
 }
 
 module_init(init_lib);
