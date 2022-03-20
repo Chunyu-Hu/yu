549b9a1bb62daa2b4f11a4db00d2af1cea2b37f9
Chunyu Hu <chuhu@redhat.com>
2022-03-20 12:09:52 +0800

    add memo for qt_5.6.1
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

--- memo/compile/arm_qt_5.6.3_compile.sh
+++ memo/compile/arm_qt_5.6.3_compile.sh
@@ -16,7 +16,10 @@ make install -j12
 
 cd /home/chuhu/Downloads/qt-everywhere-opensource-src-5.6.3.gcc-4.8.3.arm/qt-everywhere-opensource-src-5.6.3/
 export LD_LIBRARY_PATH=/opt/arm/gcc-4.8.3/lib:/opt/gmp-4.3.2/lib:/opt/mpfr-2.4.2/lib:/opt/mpc-0.8.1/lib:/opt/cloog-0.18.1/lib:/opt/openssl-1.0.2/lib:/opt/arm/openssl-1.0.2/lib
-./configure -release -opensource -xplatform linux-arm-gnueabi-g++ -prefix /opt/arm/qt-5.6.3_gcc4_8_3 -no-opengl -no-dbus -qreal float
+# Note add the tslib path to configure:
+# -L/opt/arm/tslib-1.4.gcc_4_8_3/lib
+#./configure -release -opensource -xplatform linux-arm-gnueabi-g++ -prefix /opt/arm/qt-5.6.3_gcc4_8_3 -no-opengl -no-dbus -qreal float -L/opt/arm/tslib-1.4.gcc_4_8_3/lib
+./configure -release -opensource -xplatform linux-arm-gnueabi-g++ -prefix /opt/arm/qt-5.6.3_gcc4_8_3 -no-opengl -no-dbus -qreal float -L/opt/arm/tslib-1.0.2/lib
 make -j12 V=1
 make install -j12 V=1
 ls
