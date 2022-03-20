6e3f8dd5c34a5e87b50d42d3305a6489e989c4b8
Chunyu Hu <chuhu@redhat.com>
2022-03-20 12:11:39 +0800

    add memo for arm-qt-5.7.1 compile
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

---
+++ memo/compile/arm_qt_5.7.1_compile.sh
@@ -0,0 +1,2 @@
+ ./configure -release -opensource -xplatform linux-armv7-g++ -prefix /opt/arm/qt-5.7.1.gcc4_8_3 -no-opengl -no-dbus -qreal float -tslib -verbose -L /opt/arm/tslib-1.4.gcc_4_8_3/lib  -I/opt/arm/tslib-1.4.gcc_4_8_3/include
+
