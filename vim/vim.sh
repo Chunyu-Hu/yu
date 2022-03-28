f452802d86376df73c815c396481dc6ed65e6cd9
Chunyu Hu <chuhu@redhat.com>
2022-03-28 11:19:49 +0800

    memo: add vim folder
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

---
+++ vim/vim.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# http://vim-scripts.org/vim/scripts.html
+# https://vim.sourceforge.io/scripts/script_search_results.php?&order_by=rating&show_me=20&result_ptr=0
+# https://blog.csdn.net/hdying/article/details/61422633
