ce083b3c2c40a2698b4437fca289a3e3b3722e84
Chunyu Hu <chuhu@redhat.com>
2022-04-17 16:18:03 +0800

    memo: add ycm cmdline
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

---
+++ memo/ycm/color_coded_dependency.sh
@@ -0,0 +1,3 @@
+sudo yum -y install lua-devel                                                                                          │
+sudo yum -y install llvm7.0-devel llvm9.0-devel                                                                        │
+sudo yum -y install ncurses-compat-libs                                                                                │
