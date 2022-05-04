8110510b9cf3c9332fec4c3f179b950c14df2d31
Chunyu Hu <huchunyu0312@163.com>
2022-05-04 15:47:48 +0800

    backup qt and qwt compile commands
    Signed-off-by: Chunyu Hu <huchunyu0312@163.com>

---
+++ memo/compile/qt_compile_5.7.1.sh
@@ -0,0 +1,2 @@
+../qt-everywhere-opensource-src-5.7.1/configure -qt-xcb -prefix /opt/x86_64/qt-5.7.1 -qt-sql-mysql -qt-sql-sqlite -qt-sql-sqlite2 -no-openssl --verbose
+
---
+++ memo/qwt_example_compile.sh
@@ -0,0 +1,2 @@
+#/home/chuhu/Downloads/qwt-6.1.2/examples/refreshtest
+qmake -project QT+="widgets opengl" INCLUDEPATH+=/opt/x86_64/qwt-6.1.2.gcc_4_8_5/include/ LIBS+=" -L /opt/x86_64/qwt-6.1.2.gcc_4_8_5/lib -l qwt"
